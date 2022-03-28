795caca8dd7fe1235e99d287edbe8c68c93566b8
Mauro Pandolfo <pandolfomauro@gmail.com>
2022-03-28 17:21:33 -0300

    first commit

---
+++ src/ar/edu/asap/practica0/modelo/PiedraPapelTijeraFactory.java
@@ -0,0 +1,10 @@
+package ar.edu.asap.practica0.modelo;
+
+public abstract class PiedraPapelTijeraFactory {
+	public static final int PIEDRA=1;
+	public static final int PAPEL=2;
+	public static final int TIJERA=3;
+	
+	
+	
+}
