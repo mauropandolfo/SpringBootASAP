6ac973093596b28772ac3dd85d940f393251eefd
Mauro Pandolfo <pandolfomauro@gmail.com>
2022-03-31 17:13:34 -0300

    Listado added

--- HolaSpringFrameWork/src/main/java/com/domain/controller/IndexController.java
+++ HolaSpringFrameWork/src/main/java/com/domain/controller/IndexController.java
@@ -1,6 +1,10 @@
 package com.domain.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -12,6 +16,23 @@ public class IndexController {
 	@RequestMapping("/")
 	public String goPresentacion() {
 		return "Presentacion";
+	}
+	@RequestMapping("/listado")
+	public String goListado(Model model){
+		List<String> alumnos = new ArrayList<String>();
+		
+		alumnos.add("Juan");
+		alumnos.add("Pedro");
+		alumnos.add("Maria");
+		alumnos.add("Susana");
+		
+		model.addAttribute("titulo", "Listado de alumnos");
+		model.addAttribute("profesor", "Gabriel Casas");
+		model.addAttribute("alumnos", alumnos);
+		
+		return "Listado";
+		
+		
 	}
 	
 }
