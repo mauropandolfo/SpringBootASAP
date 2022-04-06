e515160df69a1d4f998c84115f6832bee3854572
Mauro Pandolfo <pandolfomauro@gmail.com>
2022-04-06 17:15:36 -0300

    DAO Correjido

--- HolaSpringFrameWork/src/main/java/com/domain/modelo/dao/AlumnoDAO.java
+++ HolaSpringFrameWork/src/main/java/com/domain/modelo/dao/AlumnoDAO.java
@@ -7,51 +7,51 @@ import java.util.List;
 
 import com.domain.modelo.Alumno;
 import com.domain.modelo.Model;
+
+
 import com.domain.util.ConnectionManager;
 
-public class AlumnoDAO implements DAO{
-	private Connection conexion;
-	public AlumnoDAO() throws ClassNotFoundException, SQLexception{
-		ConnectionManager.conectar();
-		conexion = ConnectionManager.getConection();
+public class AlumnoDAO implements DAO {
+
+	public AlumnoDAO() throws ClassNotFoundException, SQLException{
+
 	}
 	
+	
 	@Override
-	public void agregar(Model pModel) throws ClassNotFoundException, SQLException{
-		StringBuilder sql = new StringBuilder("insert into Alumnos (ALU_NOMBRE, ALU_APELLIDO, ALU_EMAIL,");
-		sql.append("ALU_CONOCIMIENTOS, ALU_GIT)")
-		.append("values(?,?,?,?,?)");
+	public void agregar(Model pModel) throws ClassNotFoundException, SQLException {
+		ConnectionManager.conectar();
+		Connection con = ConnectionManager.getConection();
 		
-		Alumno alu =(Alumno)pModel;
-		PreparedStatement stm = conexion.prepareStatement(sql.toString());
+		String sql = new String("insert into alumnos(ALU_NOMBRE, ALU_APELLIDO, ALU_EMAIL ,ALU_CONOCIMIENTOS, ALU_GIT");
+		
+		Alumno alu = (Alumno) pModel;
+		PreparedStatement stm = con.prepareStatement(sql);
+		
 		stm.setString(1, alu.getNombre());
 		stm.setString(2, alu.getApellido());
 		stm.setString(3, alu.getEmail());
 		stm.setString(4, alu.getEstudios());
 		stm.setString(5, alu.getLinkArepositorio());
 		
-		stm.execute();
-		
 	}
 
 	@Override
-	public void modificar(Model pModel) {
+	public void modificar(Model pModel) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public void eliminar(Model pModel) {
+	public void eliminar(Model pModel) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
-	public List<Model> leer(Model pModel) {
+	public List<Model> leer(Model pModel) throws ClassNotFoundException, SQLException {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-
-	
 }
--- HolaSpringFrameWork/src/main/java/com/domain/modelo/dao/DAO.java
+++ HolaSpringFrameWork/src/main/java/com/domain/modelo/dao/DAO.java
@@ -1,11 +1,13 @@
 package com.domain.modelo.dao;
+
 import java.sql.SQLException;
 import java.util.List;
 
 import com.domain.modelo.Model;
+
 public interface DAO {
-	public void agregar (Model pModel) throws ClassNotFoundException, SQLException;
-	public void modificar(Model pModel);
-	public void eliminar(Model pModel);
-	public List<Model> leer(Model pModel);
+	public void agregar(Model pModel) throws ClassNotFoundException, SQLException;
+	public void modificar(Model pModel) throws ClassNotFoundException, SQLException;
+	public void eliminar(Model pModel) throws ClassNotFoundException, SQLException;
+	public List<Model>leer(Model pModel)throws ClassNotFoundException, SQLException;
 }
